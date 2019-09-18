0ff8eff3acbe72b49729ef924014e40ac86883d5
MarioKordic10 <55487653+MarioKordic10@users.noreply.github.com>
2019-09-18 12:28:33 +0200

    Add files via upload

---
+++ eureukune.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main ()
+{
+	float a,b,c;
+	cout<<"Unesi iznos u eurima. "<<endl;
+	cin>>a;
+	b=7,4;
+	c=b*a;
+	cout<<"iznos u kunama je "<<c<<endl;
+	system ("pause");
+	return 0;
+}
