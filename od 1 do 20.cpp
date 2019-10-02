428eb46bf57c8f5eef1be5ed5defce686830bf6e
MarioKordic10 <55487653+MarioKordic10@users.noreply.github.com>
2019-10-02 12:50:46 +0200

    Add files via upload

---
+++ od 1 do 20.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+	int brojac;
+	cout<<"ispis brojeva od 1 do 20:"<<endl;
+	for (brojac=1; brojac<=20; brojac++)
+	{
+		cout<<brojac<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
