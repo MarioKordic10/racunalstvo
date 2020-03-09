f25eaabfbf74a6de3a2c4245bfb2dcd705fa268a
mariokordic11 <55487653+mariokordic11@users.noreply.github.com>
2020-03-09 13:56:21 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,30 @@
+//IFSTREAM DATOTEKA = niz.txt
+//OFSTREAM DATOTEKA = sumabezpon.txt
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
-
 using namespace std;
-int main() {
-	//Znakovni niz
-    int i;
-	char X[]="Racunalstvo zadaca";
-	for(i=0;i<18;i++) 
-	{
-    	cout<<"X["<<i<<"]="<<X[i]<<" "<<endl;
-	}
-	
-	//Prvih 7 znakova
-	for(i=0;i<7;i++)
-	{
-	    cout<<"X["<<i<<"]="<<X[i]<<" "<<endl;
-	}
-	
-	//Naopaki niz
-for(i=18;i>=0;i--)
-   {
-        cout<<"X["<<i<<"]="<<X[i]<<" "<<endl;
-   }
-   return 0;
-   
-    	
+int main (){
+	ifstream ulaz("D:\\Mario_kordic\\zadatak.txt");
+	ofstream izlaz ("D:\\Mario_kordic\\izlazbrojeva.txt");
+	 int a[10], suma=0;
+ bool dupli[10]={false};
+ 					//	for (int i=0; i<10; i++) cout << dupli[i] << " - "; 
+ for (int i=0; i<10; i++) ulaz >>a[i];
+ 
+ for (int i=0; i<9; i++)
+     for (int j=i+1; j<10; j++) 
+      if (a[i]==a[j]) dupli[j]=true;
+ 
+ for (int i=0; i<10; i++) 
+     if (dupli[i]==false) {
+	 	suma=suma+a[i];
+     	izlaz << a[i]<< " ";
+	 }
+ izlaz << endl << "Suma bez ponavljajucih: "<< suma;
+ system ("pause");
+ return 0; 
 }
+
+
+
